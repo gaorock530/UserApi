acf11e21cb94da0a3de208e35207e450c79bd59d
gaorock530 <gaorock530@hotmail.com>
2018-04-13 20:41:11 +0800

    config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
 const env = process.env.NODE_ENV || 'test';
+console.log(`'Environment: ' + env`);
 
-if ( env === 'development' || env === 'test'){
+if ( env === 'production' || env === 'test'){
   var config = require('./config.json');
   var envConfig = config[env];
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
+    console.log(`${key}: ${envConfig[key]}`);
   });
 }
 
-console.log('Environment: ' + env);
+
